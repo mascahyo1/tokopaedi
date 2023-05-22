cee7a28935a3cb9afac68349d60ac88429401fd6
arifcahyo09 <arif.0009@students.amikom.ac.id>
2023-05-22 16:55:37 +0700

    breadcrumb halaman tentang kami dan produk

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container mx-auto ">
+            <breadcrumbs :menu="[{name:'About',url:'/about'}]"/>
+        <div class="text-xl text-bold text-center dark:text-white mb-3 px-4">
+            Tentang Kami
+        </div>
+        <div class="dark:text-white text-justify mb-3 px-4">
+            Tokopaedi adalah sebuah platform e-commerce yang didedikasikan untuk menyediakan berbagai kebutuhan sehari-hari dengan kenyamanan berbelanja secara online. Kami percaya bahwa berbelanja dapat menjadi pengalaman yang menyenangkan, praktis, dan terjangkau bagi semua orang.
+
+Di Tokopaedi, kami menawarkan beragam produk dari berbagai kategori seperti elektronik, fashion, peralatan rumah tangga, kecantikan, perlengkapan bayi, dan banyak lagi. Kami berkomitmen untuk menyediakan pilihan produk berkualitas dengan harga yang bersaing, sehingga pelanggan kami dapat menemukan apa pun yang mereka cari dengan mudah dan sesuai dengan anggaran mereka.
+
+Keunggulan Tokopaedi terletak pada pengalaman berbelanja yang aman, nyaman, dan andal. Kami bekerja sama dengan mitra pengiriman terpercaya untuk memastikan produk yang dipesan tiba tepat waktu dan dalam kondisi yang baik. Selain itu, kami menyediakan sistem pembayaran yang fleksibel dan aman, sehingga pelanggan dapat melakukan transaksi dengan tenang.
+
+Kami mengutamakan kepuasan pelanggan dan selalu siap memberikan dukungan yang dibutuhkan. Tim layanan pelanggan kami yang profesional dan responsif siap membantu menjawab pertanyaan, menangani keluhan, atau memberikan saran dengan ramah dan cepat. Kami percaya bahwa komunikasi yang baik dengan pelanggan adalah kunci untuk membangun hubungan jangka panjang yang saling menguntungkan.
+
+Tokopaedi juga aktif dalam menjaga keberlanjutan lingkungan. Kami berkomitmen untuk mengurangi dampak negatif terhadap lingkungan melalui praktik bisnis yang bertanggung jawab dan berkelanjutan. Kami berusaha untuk mengurangi penggunaan kemasan sekali pakai, mendukung produsen lokal yang berorientasi ramah lingkungan, dan mengadopsi kebijakan yang mendukung praktik bisnis yang berkelanjutan.
+
+Kami bangga menjadi bagian dari komunitas online yang tumbuh pesat dan berkomitmen untuk terus meningkatkan pengalaman berbelanja online bagi pelanggan kami. Tokopaedi hadir untuk menyediakan solusi praktis dalam memenuhi kebutuhan sehari-hari dengan kepercayaan, kualitas, dan layanan yang unggul.
+
+Jadi, ayo bergabung dengan Tokopaedi dan nikmati pengalaman berbelanja yang mudah, aman, dan menyenangkan!
+
+
+
+
+
+        </div>
+    </div>
+</template>
---
+++ pages/products.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <div class="container mx-auto">
+            <breadcrumbs :menu="[{name:'Products',url:'/products'},{name:'All', url:'#'}]"/>
+            <div class="flex justify-center flex-wrap">
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto  p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto  p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto  p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+                <div class="w-full mx-auto p-4 md:w-1/3 lg:w-1/4">
+                    <products-card/>
+                </div>
+            </div>
+        
+            
+            <div class="text-center mx-auto">
+                <nav aria-label="Page navigation example">
+                    <ul class="inline-flex items-center -space-x-px">
+                        <li>
+                        <a href="#" class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Previous</span>
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                        </a>
+                        </li>
+                        <li>
+                        <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                        </li>
+                        <li>
+                        <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                        </li>
+                        <li>
+                        <a href="#" aria-current="page" class="z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                        </li>
+                        <li>
+                        <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                        </li>
+                        <li>
+                        <a href="#" class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                        </li>
+                        <li>
+                        <a href="#" class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span class="sr-only">Next</span>
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                        </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    
+</script>
